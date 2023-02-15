3390bac24da60db4e335465847da0a940b9483ac
Kirill <pak.kiruha@yandex.ru>
2023-02-15 23:02:09 +0300

    add Load Balancer for eclient microservice

--- eclient/src/main/java/ru/twozeros/eclient/controller/HelloController.java
+++ eclient/src/main/java/ru/twozeros/eclient/controller/HelloController.java
@@ -1,5 +1,6 @@
 package ru.twozeros.eclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class HelloController {
+    @Value("${eureka.instance.instance-id}")
+    private String id;
 
     @GetMapping("/hello")
     public String sayHello() {
         return "hello world";
     }
+    @GetMapping("/instance")
+    public String getInstance() {
+        return "instance: " + id;
+    }
+
 }
